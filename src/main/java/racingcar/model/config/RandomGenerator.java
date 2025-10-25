b0ec6ccc3106a46c057432add8d0b00b0f863dad
jisooou <jisoou4@gmail.com>
2025-10-25 13:23:44 +0900

    feat(RandomGenerator): 제공된 API 활용하여 랜덤 숫자 추출

--- src/main/java/racingcar/model/config/RandomGenerator.java
+++ src/main/java/racingcar/model/config/RandomGenerator.java
@@ -1,4 +1,9 @@
 package racingcar.model.config;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomGenerator {
+    public int generateRandomNumber(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
 }
