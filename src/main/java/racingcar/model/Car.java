412a15bbbce8a64dc050d3fd1f93d2ff2c9af665
jisooou <jisoou4@gmail.com>
2025-10-25 12:58:28 +0900

    feat(Car): 자동차 이름 변수와 예외처리 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,23 @@
 package racingcar.model;
 
 public class Car {
+    private String carName;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public Car(String carName){
+        validCarName(carName);
+        this.carName = carName;
+    }
+
+    public void validCarName(String carName){
+        if(carName == null || carName.isBlank()){
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+        if(carName.length() > 5){
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
 }
