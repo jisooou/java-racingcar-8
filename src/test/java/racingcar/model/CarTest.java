ebd6698b2e21a8e9b6fcd4b67a34d7641396b1f2
jisooou <jisoou4@gmail.com>
2025-10-25 22:28:52 +0900

    test(CarTest): 자동차 이름 예외처리에 대한 테스트 코드 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,5 +1,22 @@
 package racingcar.model;
 
-public class Car {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarTest {
+    @Test
+    void 이름_5자가_넘어가면_예외반환_테스트() {
+        assertThatThrownBy(() -> new Car("javajava"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 이름_null_또는_empty_예외반환_테스트(String carName) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
