1c9ca2fbe5df01ce5c708e7a104415960d1e7789
jisooou <jisoou4@gmail.com>
2025-10-25 14:54:06 +0900

    feat(TotalCount): 자동차 전진 총 횟수에 따른 우승자 추출 코드 추가

--- src/main/java/racingcar/model/TotalCount.java
+++ src/main/java/racingcar/model/TotalCount.java
@@ -1,4 +1,26 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TotalCount {
+
+    public List<Car> findMaxCarMoveCount(List<Car> carList){
+        int maxLength = 0;
+        List<Car> carWinners = new ArrayList<>();
+
+        for(Car car : carList){
+            int length = car.moveHistoryByStep().length();
+            if (length > maxLength){
+                maxLength = length;
+            }
+        }
+
+        for(Car car : carList){
+            if(maxLength == car.moveHistoryByStep().length()){
+                carWinners.add(car);
+            }
+        }
+        return carWinners;
+    }
 }
