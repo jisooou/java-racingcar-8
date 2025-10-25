e5ac309e38a10f8949500f45f1b165683529e131
jisooou <jisoou4@gmail.com>
2025-10-25 15:43:51 +0900

    feat(CarController): 실행을 위해 메서드 호출

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,4 +1,39 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.CarMoveCount;
+import racingcar.model.TotalCount;
+import racingcar.model.config.RandomGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CarController {
+//    InputView 호출
+//    CarMoveCount 호출 - Car와 RandomGenerator 호출됨
+//    TotalCount 호출
+//    OutputView 호출
+    private final InputView inputView = new InputView();
+    private final CarMoveCount carMoveCount = new CarMoveCount(new RandomGenerator());
+    private final TotalCount totalCount = new TotalCount();
+    private final OutputView outputView = new OutputView();
+
+    public void runMain(){
+        List<String> carNames = inputView.readCarNameInput();
+        int tryCount = inputView.readTryCountInput();
+
+        List<Car> carList = carNames.stream()
+                .map(Car::new)
+                .toList();
+
+        for (int i = 0; i < tryCount; i++){
+            carMoveCount.moveCar(carList);
+            outputView.printEachStep(carList);
+        }
+
+        List<Car> carWinners = totalCount.findMaxCarMoveCount(carList);
+        outputView.printWinners(carWinners);
+    }
 }
