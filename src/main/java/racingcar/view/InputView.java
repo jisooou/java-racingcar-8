274e2d7483b5ddb836b1a857db97aacb0162429a
jisooou <jisoou4@gmail.com>
2025-10-25 13:15:59 +0900

    refactor(InputView): 입력값 String에서 List 컬렉션으로 수정 및 쉼표 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,14 +2,22 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static final String INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_PROMPT_NUMBER = "시도할 횟수는 몇 회인가요?";
 
-    public String readCarNameInput(){
+    public List<String> readCarNameInput(){
         System.out.println(INPUT_PROMPT);
-        return Console.readLine();
+        String input = Console.readLine();
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
+
     public String readTryCountInput(){
         System.out.println(INPUT_PROMPT_NUMBER);
         return Console.readLine();
