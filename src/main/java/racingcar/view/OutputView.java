5c597a63dd10273a4afd6b0ec881da4632ffdd22
jisooou <jisoou4@gmail.com>
2025-10-25 15:02:31 +0900

    feat(OutputView): 매 턴 실행 결과 및 우승자 출력 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,22 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    public void printEachStep(List<Car> carList){
+        for (Car car : carList){
+            System.out.println(car.getCarName() + " : " + car.moveHistoryByStep());
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<Car> carList){
+        String carWinners = carList.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + carWinners);
+    }
 }
