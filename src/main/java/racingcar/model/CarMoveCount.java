d5710f8d21198129f2734e3c536c17a188ae343c
jisooou <jisoou4@gmail.com>
2025-10-25 14:22:30 +0900

    feat(CarMoveCount): 랜덤 숫자 값에 따라 자동차 전진 기능 추가

--- src/main/java/racingcar/model/CarMoveCount.java
+++ src/main/java/racingcar/model/CarMoveCount.java
@@ -1,4 +1,22 @@
 package racingcar.model;
 
+import racingcar.model.config.RandomGenerator;
+
+import java.util.List;
+
 public class CarMoveCount {
+    private final RandomGenerator randomGenerator;
+
+    public CarMoveCount(RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+    public void moveCar(List<Car> carList){
+        for(Car car : carList){
+            int randomNumber = randomGenerator.generateRandomNumber();
+            if (randomNumber >= 4){
+                car.move();
+            }
+        }
+    }
 }
